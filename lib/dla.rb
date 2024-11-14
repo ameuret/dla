96380e53539f1e33c15ab5cace27039a46f942e8
Arnaud Meuret <arnaud@meuret.net>
2024-11-14 12:22:08 +0900

    Added _some more_ stuff

--- lib/dla.rb
+++ lib/dla.rb
@@ -1,16 +1,106 @@
 # frozen_string_literal: true
 
-require_relative "dla/version"
+require_relative 'dla/version'
 
 module DLA
   class Error < StandardError; end
-  class Node
-    attr_accessor :maxX
-    attr_accessor :maxY
 
-    def initialize(maxX = 1280, maxY = 720)
-      @maxX = maxX
-      @maxY = maxY
+  class Node
+    attr_accessor :xBound, :yBound, :root, :x, :y, :parent, :nodes, :ix, :iy, :history
+
+    def self.root(x: nil, y: nil, xBound: 1280, yBound: 720)
+      r = Node.new root: nil
+      r.x = r.ix = x || xBound / 2
+      r.y = r.iy = y || yBound / 2
+      r.nodes << r
+      r
+    end
+
+    def initialize(root:, xBound: 1280, yBound: 720)
+      @root = root || self
+      @xBound = xBound
+      @yBound = yBound
+      @nodes = []
+      @x, @y = randomFreePos
+      @ix, @iy = @x, @y
+      @children = {}
+      @history = String.new
+    end
+
+    def at(x, y)
+      allTreeNodes.find { |n| n.x == x && n.y == y }
+    end
+
+    def north
+      @children[:north]
+    end
+
+    def south
+      @children[:south]
+    end
+
+    def east
+      @children[:east]
+    end
+
+    def west
+      @children[:west]
+    end
+
+    def allTreeNodes
+      @root.nodes
+    end
+
+    def attach(node, position)
+      raise ArgumentError, 'position must be :north, :south, :east, :west' unless [:north, :south, :east, :west].include? position
+
+      @children[position] = node
+      node.parent = self
+      raise ArgumentError, 'Node is already present' if allTreeNodes.include? node
+
+      @root.nodes << node
+    end
+
+    def randomFreePos(forcedX: nil, forcedY: nil)
+      x = y = 0
+      loop do
+        x = rand @xBound
+        y = rand @yBound
+        break unless @root.at x, y
+      end
+      [x, y]
+    end
+
+    def oppositeDirection(dir)
+      {
+        north: :south,
+        south: :north,
+        east: :west,
+        west: :east
+      }[dir]
+    end
+
+    def move
+      raise RuntimeError if @parent
+
+      dir = [:north, :south, :east, :west].sample
+      destV = {
+        north: [0, 1],
+        south: [0, -1],
+        east: [1, 0],
+        west: [-1, 0]
+      }[dir]
+      @history << dir.to_s[0]
+      if (contact = @root.at(@x + destV[0], @y + destV[1]))
+        contact.attach(self, oppositeDirection(dir))
+      else
+        @x += destV[0]
+        @y += destV[1]
+      end
+    end
+
+    def to_s
+      format('%d,%d(%d,%d)%s', @x, @y, @ix, @iy, @history)
     end
   end
 end
--- spec/dla_spec.rb
+++ spec/dla_spec.rb
@@ -1,17 +1,109 @@
 # frozen_string_literal: true
 
-RSpec.describe DLA::Node, '#initialize' do
-  it 'takes bounds as optional arguments' do
-    expect {DLA::Node.new 42, 43}.not_to raise_error
-    # expect(n.maxX).to eq(42)
-    # expect(n.maxY).to eq(43)
-  end
-end
-
 RSpec.describe DLA::Node do
-  it 'keeps its bounds within maxX and maxY' do
-    n = DLA::Node.new 42, 43
-    expect(n.maxX).to eq(42)
-    expect(n.maxY).to eq(43)
+  before :example do
+    @root = DLA::Node.root
+  end
+
+  describe '.root' do
+    it 'creates a new root' do
+      r = DLA::Node.root
+      expect(r.root).to be r
+    end
+
+    it 'can be created at a specific point' do
+      r = DLA::Node.root(x: 42, y: 84)
+      expect(r.x).to eq(42)
+      expect(r.y).to eq(84)
+    end
+
+    it 'is centered in the bounds by default' do
+      r = DLA::Node.root
+      expect(r.x).to eq(640)
+      expect(r.y).to eq(360)
+      r = DLA::Node.root(xBound: 420, yBound: 840)
+      expect(r.x).to eq(210)
+      expect(r.y).to eq(420)
+    end
+  end
+
+  describe '#allTreeNodes' do
+    it 'lists _all_ nodes of the tree' do
+      n1 = DLA::Node.new root: @root
+      n2 = DLA::Node.new root: @root
+      n3 = DLA::Node.new root: @root
+      n4 = DLA::Node.new root: @root
+      n5 = DLA::Node.new root: @root
+      @root.attach n1, :north
+      @root.attach n2, :south
+      @root.attach n3, :east
+      n3.attach n4, :south
+      n2.attach n5, :south
+
+      allNodes = n3.allTreeNodes
+      [n1, n2, n3, n4, n5].each do
+        |n|
+        expect(allNodes.include?(n)).to be true
+      end
+    end
+  end
+
+  describe '#free?' do
+    it 'is true if the given position is unoccupied' do
+      @root.free? 10, 10
+    end
+  end
+
+  describe '#attach' do
+    it 'artificially grafts a node to an anchor point (:north, :south, :east, :west)' do
+      n = DLA::Node.new root: @root
+      @root.attach n, :south
+      expect(@root.south).to be(n)
+      expect(n.parent).to be(@root)
+    end
+  end
+
+  describe '.new' do
+    it 'takes bounds as optional arguments' do
+      DLA::Node.new root: @root, xBound: 42, yBound: 43
+    end
+
+    it 'creates a node in a random location' do
+      n = DLA::Node.new root: @root
+      expect(n.x).to be_an Integer
+      expect(n.y).to be_an Integer
+    end
+  end
+
+  it 'keeps its bounds within xBound and yBound' do
+    n = DLA::Node.new root: @root, xBound: 42, yBound: 43
+    expect(n.xBound).to eq(42)
+    expect(n.yBound).to eq(43)
+  end
+
+  describe '#move' do
+    it 'moves to a random neighboring location' do
+      n = DLA::Node.new root: @root
+      prevX, prevY = n.x, n.y
+      n.move
+      expect(n.x == prevX + 1 ||
+             n.x == prevX - 1 ||
+             n.y == prevY + 1 ||
+             n.y == prevY - 1).to be true
+    end
+
+    it 'raises an exception if asked to moved when already attached' do
+      n = DLA::Node.new root: @root
+      @root.attach n, :south
+      expect {n.move}.to raise_exception RuntimeError
+    end
+  end
+
+  describe '#to_s' do
+    it 'dumps the node state into a string like 727,187(726,187)wsenwnnew' do
+      n = DLA::Node.new root: @root
+      42.times{n.move}
+      expect(n.to_s).to match /\d+,\d+\(\d+,\d+\)[nsew]+/
+    end
   end
 end
